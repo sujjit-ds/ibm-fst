011078b8d9481b59196cc91dd4b46a7b4c7e3d28
sujjit-ds <113819884+sujjit-ds@users.noreply.github.com>
2024-05-03 16:51:19 +0530

    Cucumber

---
+++ testing fundamentals/Cucumber/testRunner/ActivitiesRunner.java
@@ -0,0 +1,16 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "features",
+        glue = {"stepDefinitions"},
+        tags = "@activity1"
+)
+
+public class ActivitiesRunner {
+    //empty
+}
